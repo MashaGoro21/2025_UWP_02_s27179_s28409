6a4240bc2de684b3471eccf4c9016b7a34bf01b6
MashaGoro21 <163156012+MashaGoro21@users.noreply.github.com>
2025-03-12 11:38:35 +0100

    Added code

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : IInteractable
+{
+    public void AddToTheInventory()
+    {
+        throw new System.NotImplementedException();
+    }
+}
